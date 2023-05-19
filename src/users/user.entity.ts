6806ef89c826ce21c5334017db178e6f9259204c
Chris Halabi <chridtianhalabi@gmail.com>
2023-05-19 10:48:30 +0300

    push before ubuntu reset

---
+++ src/users/user.entity.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty()
+  @Column()
+  firstName: string;
+
+  @ApiProperty()
+  @Column()
+  lastName: string;
+
+  @ApiProperty()
+  @Column({ nullable: true })
+  image: string;
+}
